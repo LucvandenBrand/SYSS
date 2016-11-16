6aae75e055cd60330c94da46bd333f566aa89663
thijs knaap <thijs knaap>
2016-11-16 12:48:48 +0100

    the block moves :O

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rb;
+
+    public float moveSpeed;
+    public float rotationSpeed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        // Only allow rotation in the y rotation
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+    }
+
+    /*Manages the movement and rotation of the player*/
+	void FixedUpdate()
+    {
+        RotateAction();
+        MoveAction();
+    }
+
+    /*Manages the forward and backward movement*/
+    private void MoveAction()
+    {
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(0.0f, 0.0f, moveVertical * moveSpeed * Time.deltaTime);
+        rb.AddRelativeForce(movement);
+    }
+
+    /*Manages all the rotation*/
+    private void RotateAction()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 rotation = new Vector3(0.0f, moveHorizontal * rotationSpeed, 0.0f);
+        rb.AddRelativeTorque(rotation * Time.deltaTime);
+    }
+}
