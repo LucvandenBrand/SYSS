ea262204be7ad03cc25707914fd7ef10126d23ea
Luc van den Brand <Lukeslux@users.noreply.github.com>
2016-11-16 17:02:53 +0100

    Added wireframe shading.

---
+++ Assets/Scripts/Camera/WireframeCamera.cs
@@ -0,0 +1,21 @@
+﻿/* Makes the camera render in wireframe mode using the provided color and width. */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class WireframeCamera : MonoBehaviour {
+    public float wireWidth = 0.1f;
+    public Color wireColor = Color.white;
+
+	void Start () {
+        Camera camera = GetComponent<Camera>();
+        camera.SetReplacementShader(Shader.Find("Custom/Wireframe"), null);
+    }
+	
+	// Called right before the shader renders.
+	void OnPreRender () {
+        Shader.SetGlobalColor("_LineColor", wireColor);
+        Shader.SetGlobalFloat("_LineWidth", wireWidth);
+    }
+}
