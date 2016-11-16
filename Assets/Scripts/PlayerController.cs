5f67d206cd5aed4c99550b9beeb285a4afe330db
thijs knaap <thijs knaap>
2016-11-16 17:53:37 +0100

    controls for 2 players

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     private Rigidbody rb;
 
+    public string MoveKeysName;
+    public string rotateKeysName;
     public float moveSpeed;
     public float rotationSpeed;
 
@@ -25,7 +27,7 @@ public class PlayerController : MonoBehaviour {
     /*Manages the forward and backward movement*/
     private void MoveAction()
     {
-        float moveVertical = Input.GetAxis("Vertical");
+        float moveVertical = Input.GetAxis(MoveKeysName);
 
         Vector3 movement = new Vector3(0.0f, 0.0f, moveVertical * moveSpeed * Time.deltaTime);
         rb.AddRelativeForce(movement);
@@ -34,7 +36,7 @@ public class PlayerController : MonoBehaviour {
     /*Manages all the rotation*/
     private void RotateAction()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveHorizontal = Input.GetAxis(rotateKeysName);
         Vector3 rotation = new Vector3(0.0f, moveHorizontal * rotationSpeed, 0.0f);
         rb.AddRelativeTorque(rotation * Time.deltaTime);
     }
