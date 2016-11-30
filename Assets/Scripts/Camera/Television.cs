8adbaf4fe1268cf7869995256a3db27556dfbb9c
Lukeslux <lucas.brnd@gmail.com>
2016-11-30 10:43:39 +0100

    Rectangle televisions allowed.

--- Assets/Scripts/Camera/Television.cs
+++ Assets/Scripts/Camera/Television.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Renderer))]
 public class Television : MonoBehaviour {
 	private RenderTexture cam1Texture, cam2Texture;
-	public int resolution = 128; // The resolution of the tv in pixels.
-	public float curve = 0.1f; // The curvature of the screen.
+	public int resolutionX = 1920; // The resolution width of the tv in pixels.
+    public int resolutionY = 1080; // The resolution height of the tv in pixels.
+    public float curve = 0.1f; // The curvature of the screen.
 	public Camera camera1;
 	public Camera camera2;
 	
 	// Binds the camera's to the texture.
 	void Start () {
 		GenMesh();
-		this.cam1Texture = new RenderTexture(resolution, resolution, 1);
-		this.cam2Texture = new RenderTexture(resolution, resolution, 1);
+		this.cam1Texture = new RenderTexture(resolutionX, resolutionY, 1);
+		this.cam2Texture = new RenderTexture(resolutionX, resolutionY, 1);
 		camera1.targetTexture = cam1Texture;
 		camera2.targetTexture = cam2Texture;
 		GetComponent<Renderer>().material.SetTexture("_MainTex", cam1Texture);
@@ -21,14 +23,8 @@ public class Television : MonoBehaviour {
 	}
 	
 	void GenMesh() {
-        Mesh mesh = GetComponent<MeshFilter>().mesh;
-        Vector3[] vertices = mesh.vertices;
-        Vector3[] normals = mesh.normals;
-        int i = 0;
-        while (i < vertices.Length) {
-            vertices[i][1] += Mathf.Sin(Time.time)*(0.01f*i/vertices.Length);
-            i++;
-        }
-        mesh.vertices = vertices;
+        float scale = 1;
+        float scaleX = scale*resolutionX/resolutionY;
+        transform.localScale = new Vector3(scaleX, 1, scale);
     }
 }
