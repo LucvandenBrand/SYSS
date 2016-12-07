539cde18683cfb0cb52c693b15f2b413310ebeef
thijs knaap <thijs knaap>
2016-12-07 12:14:19 +0100

    TURRET DRAAAAIT

---
+++ Assets/Scripts/BarrelPitch.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarrelPitch : MonoBehaviour {
+
+    public Transform CameraHolderVertical;
+
+    private HingeJoint hingeJoint;
+
+    void Start () {
+        hingeJoint = this.GetComponent<HingeJoint>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	}
+}
--- Assets/Scripts/TurretRotation.cs
+++ Assets/Scripts/TurretRotation.cs
@@ -3,23 +3,38 @@ using System.Collections;
 
 public class TurretRotation : MonoBehaviour {
 
-    public GameObject goTarget;
+    public Transform CameraHolderHorizontal;
     public float maxDegreesPerSecond;
-    private Quaternion qTo;
-    ConfigurableJoint cj;
 
-
-    void Start()
+    void FixedUpdate()
     {
-        cj = this.GetComponent<ConfigurableJoint>();
-    }
+        float current = transform.rotation.eulerAngles.y;
+        float target = CameraHolderHorizontal.rotation.eulerAngles.y;
 
-    void Update()
-    {
-        /*cj.angularYZDrive = 
-        Vector3 v3T = goTarget.transform.position - transform.position;
-        v3T.y = transform.position.y;
-        qTo = Quaternion.LookRotation(v3T, Vector3.up);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, qTo, maxDegreesPerSecond * Time.deltaTime);*/
+        if (target > current + 0.5 || target < current - 0.5)
+        {
+            if ((target - current + 360) % 360 > 180)
+            {
+                if (((target - current + 360) % 360) > 360 - maxDegreesPerSecond * Time.deltaTime)
+                {
+                    transform.Rotate(new Vector3(0, -((target - current + 360) % 360), 0));
+                }
+                else
+                {
+                    transform.Rotate(new Vector3(0, -maxDegreesPerSecond * Time.deltaTime, 0));
+                }
+            }
+            else
+            {
+                if (((target - current + 360) % 360) < maxDegreesPerSecond * Time.deltaTime)
+                {
+                    transform.Rotate(new Vector3(0, ((target - current + 360) % 360), 0));
+                }
+                else
+                {
+                    transform.Rotate(new Vector3(0, maxDegreesPerSecond * Time.deltaTime, 0));
+                }
+            }
+        }
     }
 }
